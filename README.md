8fb2a156f19a82dc4e7265a9f4197d8c644c441d
Zain Naqvi <zainnaqvi.200621@gmail.com>
2026-06-20 04:02:23 -0400

    QEMU EDU dev environment + hello-world kernel module

---
+++ README.md
@@ -0,0 +1,5 @@
+# QEMU EDU Linux Kernel PCI Driver
+
+A Linux kernel PCI driver for QEMU's EDU virtual device. MMIO, MSI interrupts,
+and DMA, plus a userspace library and a latency/throughput benchmark.
+Developed and tested against the EDU device inside a QEMU VM. Work in progress.
---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+qemu-system-x86_64 \
+  -machine q35 \
+  -m 2048 \
+  -smp 2 \
+  -drive file=ubuntu.img,format=qcow2,if=virtio \
+  -drive file=seed.img,format=raw,if=virtio \
+  -device edu \
+  -netdev user,id=net0,hostfwd=tcp::2222-:22 \
+  -device virtio-net-pci,netdev=net0 \
+  -virtfs local,path=$HOME/edu-driver/src,mount_tag=hostshare,security_model=none \
+  -nographic
