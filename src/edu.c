5acb13e33a135672750d58f6ea3fdc9f0dd23006
Zain Naqvi <zainnaqvi.200621@gmail.com>
2026-07-02 00:56:14 -0400

    update edu.c.
    - add file_operations struct, and fill in the fields
    - allocate memory for the edu_device struct in probe()
    - store the pointer to the edu_device struct in probe() to make it accessible in remove()
    - fill in the fields of edudev's miscdev struct which comes from the miscdevice.h library
    - register the misc device
    - add a new path in the goto error mappings
    - create the read/write functions
    
    Read/Write function definitions are next. Yet to test the full code with these new additions

--- src/edu.c
+++ src/edu.c
@@ -7,13 +7,22 @@
 #include <linux/module.h> //all kernel modules
 #include <linux/pci.h> //used to interact with PCI drivers and devices
 #include <linux/init.h> //__init and __exit macros 
+#include <linux/miscdevice.h> //used to create a misc device
+#include <linux/fs.h> //used for file operations struct
 
 MODULE_LICENSE("GPL"); 
 MODULE_AUTHOR("Zain");
 MODULE_DESCRIPTION("PCI Driver Work");
 MODULE_VERSION("1.0");
 
-static void __iomem *io_base; //This annotation allows us to store the void generic pointer type returned by ioremap, and helps in stopping compiler optimizations
+//read function: polls the EDU's status register to see if the first bit clears. Then, it reads the result in factorial computation register (0x08) 
+static ssize_t read_driver(struct file *flip, char __user *user_buf, size_t len, loff_t *offset) {
+
+}
+//write function: write into the status register (0x20) to set its first bit. Write the input number to the factorial computation register (0x08). 
+static ssize_t write_driver(struct file *flip, const char __user *user_buf, size_t len, loff_t *offset) {
+    
+}
 
 static const struct pci_device_id pci_ids[] = { //an array of the struct pci_device_id listing what we match with 
     { PCI_DEVICE(EDU_VENDOR_ID, EDU_DEVICE_ID) }, //Give the device our specifics
@@ -21,15 +30,33 @@ static const struct pci_device_id pci_ids[] = { //an array of the struct pci_dev
 };
 MODULE_DEVICE_TABLE(pci, pci_ids); //helps the kernel match hardware devices to the appropriate driver
 
-static const struct edu_device {
-    
-}
+struct edu_device {
+    struct miscdevice miscdev; //struct for the misc device (from the miscdevice.h file) 
+    //the __iomem means that the pointer is to a memory-mapped I/O region. This is used to tell the compiler that the pointer is not a normal pointer, and that it should not optimize accesses to it
+    void __iomem *io_base; //pointer to the base of the BAR0 region. This is used to read and write to the device
+};
+
+static struct file_operations fops = {
+    .owner = THIS_MODULE,
+    .read = read_driver,
+    .write = write_driver
+};
 
 //breif function is called when a PCI device is registered. It takes in dev and id information
 static int probe(struct pci_dev* dev, const struct pci_device_id* id) {
 
     u32 registerValue; //32 bit unsigned integer to store the value read from the BAR0 region
     int result;
+    int misc_result;
+
+    //using device manager kzalloc to allocate memory for the edu_device struct. 
+    struct edu_device *edudev = devm_kzalloc(&dev->dev, sizeof(*edudev), GFP_KERNEL);
+    if (!edudev) {
+        return -ENOMEM;
+    }
+
+    //STORE
+    dev_set_drvdata(&dev->dev, edudev); //store the pointer to the edu_device struct in the dev->dev->driver_data field. This is used to retrieve the struct in remove()
 
     //ENABLE
     result = pci_enable_device(dev); //takes in the pointer to the PCI device. Wakes up the device and PCI bridge, along with allocation of resources like I/O ports and memory regions
@@ -47,15 +74,26 @@ static int probe(struct pci_dev* dev, const struct pci_device_id* id) {
     } 
 
     //MAP BAR0 REGION
-    io_base = ioremap(pci_resource_start(dev, 0), pci_resource_len(dev, 0)); //Base of BAR0 is the first argument, and the length of BAR0 is the second argument. This maps the physical address of BAR0 to a virtual address in kernel space. The return value is a pointer to the virtual address, which we store in io_base
-    if (!io_base) {
+    edudev->io_base = ioremap(pci_resource_start(dev, 0), pci_resource_len(dev, 0)); //Base of BAR0 is the first argument, and the length of BAR0 is the second argument. This maps the physical address of BAR0 to a virtual address in kernel space. The return value is a pointer to the virtual address, which we store in io_base
+    if (!(edudev->io_base)) { //if NULL
         dev_err(&dev->dev, "Failed to Map\n");
         result = -ENOMEM; //macro that returns an error code for 'out of memory'
         goto err_release_region;        
     }
 
+    edudev->miscdev = (struct miscdevice){
+        .minor = MISC_DYNAMIC_MINOR,
+        .name = "misc_device",
+        .fops = &fops,
+    };
+
+    misc_result = misc_register(&edudev->miscdev); //register the misc device with the kernel. This creates a device file in /dev/misc device, which we can use to communicate with the driver
+    if (misc_result) {
+        goto err_misc_register; //goto is used to jump to the err_misc_register label, which unmaps the region, and continues with the fail-safe driver exit
+    }
+
     //io_base is a pointer type. We can not dereference it, so we end up using ioread32
-    registerValue = ioread32(io_base); //Used to read 32 bit values without the need for dereferencing a pointer. This protects us from caching and compiler optimizations
+    registerValue = ioread32(edudev->io_base); //Used to read 32 bit values without the need for dereferencing a pointer. This protects us from caching and compiler optimizations
 
     pr_info("Successfully enabled PCI device\n");
     pr_info("Read value from BAR0: 0x%08x\n", registerValue); //writes the kernel log buffer which we read with dmesg
@@ -63,6 +101,9 @@ static int probe(struct pci_dev* dev, const struct pci_device_id* id) {
     
     return 0;
 
+err_misc_register:
+    misc_deregister(&edudev->miscdev);
+    iounmap(edudev->io_base); //unmap the BAR0 region from virtual Kernel Space
 err_release_region:
     pci_release_region(dev, 0); //to avoid leaks
 err_disable_device:
@@ -72,8 +113,12 @@ err_disable_device:
 
 //brief function is called when a PCI device is unregistered. It takes in dev information
 static void remove(struct pci_dev* dev){
+    //RECOVER the struct, deregister the misc device, and continue with the fail-safe exit
+    struct edu_device *edudev = dev_get_drvdata(&dev->dev); //get the pointer to the edu_device struct that we allocated in probe. This is stored in the dev->dev->driver_data field, which we can access using dev_get_drvdata
+    misc_deregister(&edudev->miscdev); //deregister the misc device inside the edudev
+    
     //We unmap, then release the claimed memory in said order
-    iounmap(io_base); //unmap the BAR0 region from virtual Kernel Space
+    iounmap(edudev->io_base); //unmap the BAR0 region from virtual Kernel Space
     pci_release_region(dev, 0); //release the claimed BAR0 region
     pci_disable_device(dev);
     pr_info("Removed Device Successfully\n");
