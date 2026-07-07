ae524b994901e376d2944dc4368789a636e70741
Zain Naqvi <zainnaqvi.200621@gmail.com>
2026-07-07 03:31:37 -0400

    complete edu_test.c file. It opens the driver file, writes an input, and reads from the driver. It catches and prints any error from the device's end using errno

--- userspace/edu_test.c
+++ userspace/edu_test.c
@@ -1,9 +1,15 @@
 #include <fcntl.h> //used for opening the device file
 #include <unistd.h> //used for write(), read(), and close() functions
 #include <stdint.h> 
+#include <stdio.h> //used for printf()
+#include <errno.h> //used for error handling
+#include <string.h> //used for strerror()
 
 int ourFile;
-int inputNum;
+uint32_t inputNum;
+uint32_t outputNum;
+ssize_t successWrite;
+ssize_t successRead;
 
 int main() {
 
@@ -17,6 +23,22 @@ int main() {
     }
 
     //WRITE: successfully opened the file. Now, we write to it
-    write(ourFile, inputNum, sizeof(inputNum)); 
+    successWrite = write(ourFile, &inputNum, sizeof(inputNum)); //Writes the inputNum to the file. Returns the number of bytes written successfully 
+    if (successWrite == -1) { //failed to write to the file
+
+        printf("Driver write failed with error code: %s\n", strerror(errno));
+        return -1;
+
+    }
+
+    //READ: successfully wrote to the file. Now, we read from it
+    successRead = read(ourFile, &outputNum, sizeof(outputNum)); //Reads the number from the file. Returns the number read
+    if (successRead == -1 || successRead != sizeof(outputNum)) { //failed to read from the file
+
+        printf("Driver read failed with error code: %s\n", strerror(errno));
+        return -1;
+
+    }
+    printf("The output from the driver is %d\n", outputNum);
 
 }   
