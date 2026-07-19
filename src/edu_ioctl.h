e2e5d3709fb301e4d5102533e3c26178e35cb6d5
Zain Naqvi <zainnaqvi.200621@gmail.com>
2026-07-19 04:11:17 -0400

    update the identifier char to 'J' from 'z' as the latter is already taken by the CAN bus card in the ioctl documents.

--- src/edu_ioctl.h
+++ src/edu_ioctl.h
@@ -8,7 +8,7 @@ struct edu_dma_arg {
     __u64 data_ptr; //holds the address to the data payload. This is later typecasted to a void pointer which points to the data
 };
 
-#define EDU_DMA_TO_DEVICE _IOW('z', 0, struct edu_dma_arg) //This is the command that we will use to tell the driver to perform a DMA transfer from RAM to the EDU device
-#define EDU_DMA_FROM_DEVICE _IOW('z', 1, struct edu_dma_arg) //This is the command that tells the driver we want to perform a DMA transfer from the EDU device to RAM
+#define EDU_DMA_TO_DEVICE _IOW('J', 0, struct edu_dma_arg) //This is the command that we will use to tell the driver to perform a DMA transfer from RAM to the EDU device
+#define EDU_DMA_FROM_DEVICE _IOW('J', 1, struct edu_dma_arg) //This is the command that tells the driver we want to perform a DMA transfer from the EDU device to RAM
 
 #endif
