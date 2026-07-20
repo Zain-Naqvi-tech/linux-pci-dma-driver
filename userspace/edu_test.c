3e8e089b25469e549dcbbd96c4adac84b0bdb312
Zain Naqvi <zainnaqvi.200621@gmail.com>
2026-07-20 04:09:38 -0400

    added strerror(errno) to errors which would disrupt the flow of DMA. Added return statements to terminate the program on failure and success. Moved the global variables into main() as there was no need for them to be file-scoped (working on my scope-side things of coding)

--- userspace/edu_test.c
+++ userspace/edu_test.c
@@ -9,13 +9,13 @@
 
 #include "../src/edu_ioctl.h" //shared header file between the userspace and device file
 
-int ourFile;
-uint32_t inputNum;
-uint32_t outputNum;
-ssize_t successWrite;
-ssize_t successRead;
-
 int main() {
+    
+    int ourFile;
+    uint32_t inputNum;
+    uint32_t outputNum;
+    ssize_t successWrite;
+    ssize_t successRead;
 
     inputNum = 5;
 
@@ -31,6 +31,7 @@ int main() {
     int *start_buffer = (int *)malloc(4096); //allocate the userspace buffer with a known pattern
     if (start_buffer == NULL) {
         printf("Malloc Failed!\n");
+        return -1;
     }
     for (int i = 0; i < 5; i++) { //takes up 5*4 = 20 bytes of memory
         start_buffer[i] = i; //expected to be [0,1,2,3,4]m
@@ -39,23 +40,26 @@ int main() {
     int *end_buffer = (int *)malloc(4096); //allocate the userspace buffer for the read-back 
     if (end_buffer == NULL) {
         printf("Malloc Failed!\n");
+        return -1;
     }
 
     struct edu_dma_arg userspace_arg; 
     userspace_arg.size = 20;
     userspace_arg.data_ptr = (uint64_t)(unsigned long)start_buffer;
 
-    //ioctl for EDU_DMA_TO_DEVICE. userspace buffer -> CPU copies to DMA buffer (cpu_addr) -> Hardware reads from DMA buffer (dma_handle) an writes to the Device Buffer at 0x40000
+    //ioctl for EDU_DMA_TO_DEVICE. userspace buffer -> CPU copies to DMA buffer (cpu_addr) -> Hardware reads from DMA buffer (dma_handle) and writes to the Device Buffer at 0x40000
     int ioctl_result = ioctl(ourFile, EDU_DMA_TO_DEVICE, &userspace_arg); //use the ioctl function to kickstart the transfer TO the device FROM the userspace buffer
     if (ioctl_result) {
-        printf("IOCTL (1) FAILED!\n");
+        printf("IOCTL (1) FAILED with error code: %s\n", strerror(errno));
+        return -1;
     }
 
     userspace_arg.data_ptr = (uint64_t)(unsigned long)end_buffer; //now point the data pointer to the end_buffer. The buffer which will be filled in by the hardware based on what its buffer is filled with
     //ioctl for EDU_DMA_FROM_DEVICE. Device buffer at 0x40000 -> hardware reads from itself anf writes to the DMA buffer (dma_handle) -> CPU reads from cpu_addr and copies it into the userspace buffer end_buffer
     ioctl_result = ioctl(ourFile, EDU_DMA_FROM_DEVICE, &userspace_arg);
     if (ioctl_result) {
-        printf("IOCTL (2) FAILED!\n");
+        printf("IOCTL (2) FAILED with error code: %s\n", strerror(errno));
+        return -1;
     }
 
     int final_result = memcmp(start_buffer, end_buffer, userspace_arg.size);
@@ -90,5 +94,8 @@ int main() {
 
     }
     printf("The output from the driver is %d\n", outputNum);
+
+    close(ourFile); //close the file
+    return 0;
 
 }   
